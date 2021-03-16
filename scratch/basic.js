eca87bab84df887d118c195fc1d8f901fd2b4a2e
Jens Stigaard <jens@stigaard.info>
2021-03-16 17:35:22 +0100

    Updated scratch file

--- scratch/basic.js
+++ scratch/basic.js
@@ -2,13 +2,15 @@ const Vue = require('vue')
 
 const { vMixConnectionPlugin, vMixConnectionPluginStore } = require('../dist/index')
 
-
 Vue.use(vMixConnectionPlugin, new vMixConnectionPluginStore())
 
 const app = new Vue({
 	// Test that when create you can access the vMix plugin and use it
 	created() {
-		this.$vMixConnection.setConnection('192.168.0.115', { debug: true })
+		this.$vMixConnection.setConnection('localhost', { debug: false })
+		this.$vMixConnection.on('connect', () => {
+			console.log('Connected to vMix instance')
+		})
 		this.$vMixConnection.on('data', data => {
 			console.log('Received data from vMix instance')
 			console.log(data)
