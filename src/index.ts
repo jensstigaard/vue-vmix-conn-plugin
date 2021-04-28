8e187e1ac3f5e83584407d027a4461f87e47a6bd
Jens Stigaard <jens@stigaard.info>
2021-04-28 17:28:10 +0200

    Cleaned up code

--- src/index.ts
+++ src/index.ts
@@ -8,17 +8,28 @@ import { ConnectionTCP } from 'node-vmix'
 //  - https://vuejs.org/v2/guide/plugins.html
 //  - https://codepen.io/stigaard/pen/yLNNQbg
 export class vMixConnectionPluginStore {
-  storeVM: Vue = new Vue({
+  // protected supportMultipleConnections: boolean = false
+
+  protected storeVM: Vue = new Vue({
     data: {
-      connection: null,
-      connected: false,
+      connection: undefined,
     },
   })
 
-  get connection() {
+  constructor(_supportMultipleConnections: boolean = false) {
+    // this.supportMultipleConnections = _supportMultipleConnections
+  }
+
+  /**
+   * Get first connection
+   */
+  get connection(): ConnectionTCP | undefined {
     return this.storeVM!.$data.connection
   }
 
+  /**
+   * Get is connected
+   */
   get connected(): boolean {
     if (!this.connection) {
       return false
@@ -28,21 +39,38 @@ export class vMixConnectionPluginStore {
   }
 
   setConnection(host: string, options: { [key: string]: any } = {}) {
-    // Shutdown/Destroy old connection?
-    if (this.connection) {
-      console.log('Shutting down old connection', this.connection)
-      this.connection.shutdown()
+    if (!this.connection) {
+      // Set new connection
+      this.storeVM.$data.connection = new ConnectionTCP(host, options)
     }
 
-    // Set new connection
-    // @ts-ignore
-    this.storeVM!.$data.connection = new ConnectionTCP(host, options)
-    console.log('Set new connection', this.storeVM!.$data.connection)
+    // console.log('Set new connection', this.storeVM!.$data.connection)
+    // Shutdown/Destroy old connection?
+    if (this.connection && this.connection.connected()) {
+      // console.log('Shutting down old connection', this.connection)
+      this.shutdown()
+    }
+
+    this.changeConnection(host, options.port || 8099)
+  }
+
+  /**
+   * Change connection
+   * 
+   * @param host
+   * @param port
+   */
+  changeConnection(host: string, port: number) {
+    if (!this.connection) {
+      throw new Error('vMix Connection not set...')
+    }
+
+    this.connection.connect(host, port)
   }
 
   shutdown() {
     // If no connection then do nothing
-    if (!this.connection) {
+    if (!this.connection || !this.connection.connected()) {
       return
     }
 
@@ -51,19 +79,31 @@ export class vMixConnectionPluginStore {
     this.connection.shutdown()
   }
 
+  /**
+   * Send commands to vMix instance
+   * @param commands
+   */
   send(commands: any) {
     if (!this.connection) {
       throw new Error('No vMix connection instanciated')
     }
+
+    if (!this.connected) {
+      throw new Error('vMix connection not connected')
+    }
+
     this.connection.send(commands)
   }
 
-  on(...args: any) {
+  /** 
+   * Register on(...) listener to connection
+   */
+  on(type: string, cb: Function) {
     if (!this.connection) {
       throw new Error('No vMix connection instanciated')
     }
 
-    this.connection.on(...args)
+    this.connection.on(type, cb)
   }
 }
 
@@ -75,26 +115,21 @@ export const vMixConnectionPlugin = {
       },
     })
 
-    // Global method - set vMix connection
-    Vue.prototype.setVmixConnection = function (
-      host: string,
-      options: { [key: string]: any } = {},
-      keepListeners: boolean = false
-    ) {
-      if (keepListeners) {
-        console.log('Take care of listeners...')
-      }
+    // // Global method - set vMix connection
+    // Vue.prototype.setVmixConnection = function (
+    //   host: string,
+    //   options: { [key: string]: any } = {}
+    // ) {
+    //   this.$vMixConnection.setConnection(host, options)
+    // }
 
-      this.$vMixConnection.setConnection(host, options)
-    }
+    // Vue.prototype.shutdownVmixConnection = function () {
+    //   this.$vMixConnection.shutdown()
+    // }
 
-    Vue.prototype.shutdownVmixConnection = function () {
-      this.$vMixConnection.shutdown()
-    }
-
-    Vue.prototype.execVmixCommands = function (commands: any) {
-      this.$vMixConnection.send(commands)
-    }
+    // Vue.prototype.execVmixCommands = function (commands: any) {
+    //   this.$vMixConnection.send(commands)
+    // }
   },
 }
 
